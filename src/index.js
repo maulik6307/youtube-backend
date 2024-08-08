587d657a48b7a6c2e0200451f297c13e1ca9b3d0
maulik6307 <maulikbhavnagariya@gmail.com>
2024-08-08 18:19:56 +0530

    utils files =>ApiError,ApiResponse,asyncHandler added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
   path: "/.env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(` Server started on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection fail !!!", err);
+  });
 
 //THIS APPROACH IS ALSO WORK
 /*
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,29 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export default asyncHandler;
+
+//=================SECOND APPROACH=========================//
+
+/*
+
+const asyncHandler=(fun)=>async (req,res,next)=>{
+
+
+  try{
+  await fun(req,res,next)
+  }catch(err){
+  res.status(err.code || 500).json({
+  
+  resposeCode:500,
+  message:err.message
+  })
+  }
+
+  }
+
+
+*/
