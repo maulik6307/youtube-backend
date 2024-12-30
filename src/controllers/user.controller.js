5bc5d556fe2ff263bf01f6d567a785879aafa741
MAULIK BHAVNAGARIYA <bhavnagariyamaulik@gmail.com>
2024-12-30 09:51:22 +0530

    generate remaining models

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -263,7 +263,9 @@ const logoutUser = asyncHandler(async (req, res) => {
   await User.findByIdAndUpdate(
     req?.user._id,
     {
-      refreshToken: undefined,
+      $unset: {
+        refreshToken: 1,
+      }
     },
     {
       new: true,
